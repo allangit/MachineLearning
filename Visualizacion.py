7e1fa797c6ae8dfa65b321f19168f4ec79177e7a
descartes640@gmail.com <descartes640@gmail.com>
2023-07-23 21:09:13 -0600

    Modificando el Ventana y metodos de filtrado

--- Visualizacion.py
+++ Visualizacion.py
@@ -2,7 +2,12 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt 
 import mysql.connector as mariadb
-'''
+
+
+pd.set_option('display.width', 400)
+pd.set_option('display.max_columns', 70)
+
+
 class Carga:
      
     def __init__(self):
@@ -10,6 +15,8 @@ class Carga:
         self.figure=0
         self.axs=0
         self.ax=0
+        self.desired=0
+        self.all_columns=0
         self.df=pd.read_csv("Customer Churn Model.txt")
         print(self.df)
         
@@ -26,7 +33,7 @@ class Carga:
         self.df.plot(kind="scatter",x="Day Calls", y="Day Charge", ax=self.axs[1][0])
         self.df.plot(kind="scatter",x="Day Mins", y="Day Charge", ax=self.axs[0][0])
         plt.show()
-'''
+
 class data_Wranglig:
     
     def __init__(self):
@@ -37,18 +44,61 @@ class data_Wranglig:
         self.df3=0
         self.df4=0
         self.df5=0
+        self.all_columns=0
+        self.sublist=0
+        self.x=0
+        self.data1=0
+        self.data2=0
+        self.data3=0
+        self.data4=0
         
         
-    def conjuntoDatos(self):
+    def conjunto1(self):
         
         print(self.df.columns.values.tolist())
         self.df2=self.df['Account Length']
-        print(self.df2)
+        print(self.df2)  
+        
+    def conjunto2(self):
+        
+        self.df3=self.df[['Account Length', 'Phone', 'Eve Charge', 'Day Calls']]
+        print(self.df3)
+        print("X"*40)
+        
+    def conjunto3(self):
+        
+        self.desired=['Account Length', 'Area Code', 'Phone', 'Day Calls']
+        self.df4=self.df[self.desired]
+        print(self.df4)
+        
+    def conjunto4(self):
+        
+        self.all_columns=self.df.columns.values.tolist()
+        self.sublist=[self.x for self.x in self.all_columns if self.x not in self.desired]
+        print(self.sublist)
+        
+    def filtrado1(self):
+        
+        print(self.df[1:25])
+        print(self.df[20:35])
+        print(self.df[200:])
+        
+    def filtrado2(self):
+        
+        self.data2=self.df[self.df["Day Mins"] > 350]
+        print(self.data2)
+        
+        
+        
         
 
         
 data=data_Wranglig()
-data.conjuntoDatos()        
-        
+data.conjunto1()
+data.conjunto2()
+data.conjunto3()
+data.conjunto4()
+data.filtrado1()
+data.filtrado2()
         
         
